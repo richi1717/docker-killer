721e6700a5c9ddd6c472df30bd5997d0c97f35e0
Richi Jeffery <rici1717@gmail.com>
2019-04-05 18:46:17 -0700

    add script

---
+++ .docker-kill.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# color codes
+dark_gray='\033[1;30m'
+light_red='\033[1;31m'
+light_green='\033[1;32m'
+light_blue='\033[1;34m'
+light_purple='\033[1;35m'
+light_cyan='\033[1;36m'
+
+if [[ "$1" = "" ]]
+then
+  echo -e "
+  ${dark_gray}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  Please enter the IMAGE NAME you would like to 🔫 (AKA stop/kill/petrify (usually a negative status effect)
+  Something like:
+
+  ${light_green}$ ~/.docker-kill.sh hello-world
+  ${dark_gray}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  "
+else
+  echo -e "
+  ${dark_gray}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  Looking for ${light_purple}$1
+  ${dark_gray}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  "
+
+  docker_id=$(docker ps | grep $1)
+
+  arr=(${docker_id})
+  if [[ "${arr[0]}" != "" ]]
+  then
+  id=${arr[0]}
+  echo -e "${light_green}Found ${light_purple}$1${dark_gray} id: ${light_cyan}
+   "
+
+  docker kill ${id}
+
+  echo -e "
+🔫${dark_gray} Bang! Bang! 🔫
+
+${light_cyan}${id}${dark_gray} is killed!
+
+☠        RIP!!        ☠ "
+  else
+  echo -e "${light_red}No 🐳🐳 ${light_blue}Docker${light_red} 🐳🐳 image for ${purple}$1${light_red} to 🔫💀😵☠️‍"
+  fi
+fi
---
+++ README.md
@@ -0,0 +1,31 @@
+# Docker Killer
+
+A script to kill a Docker image by name.
+
+## Installation
+Either copy and paste the text from `.docker-kill.sh` or clone this repo and move to `~`.
+
+Then run
+
+```bash
+$ chmod 755 ~/.docker-kill.sh
+
+```
+
+## Use
+
+Run
+```bash
+$ ~/.docker-kill.sh YOUR_DOCKER_IMAGE_NAME_HERE
+```
+
+It will then let you know it was successful if it finds the image name.  You don't have
+to pass the entire name.  IE say you have an image name of `supercalifloweristiceggspewalleydoses`
+
+```bash
+$ ~/.docker-kill.sh supercali
+```
+
+Just make sure the name does not match a different image name.
+ 
+
